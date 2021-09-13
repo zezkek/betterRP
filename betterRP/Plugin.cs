f959990a6a59a4c1b4af62a8ff4d3339e5ce6a16
zezkek <super.vec2013@gmail.com>
2021-09-13 18:40:54 +0300

    Revert "Merge branch 'devM'"
    This reverts commit 2f357986eb63d9421e308e6b8050a0b889275128, reversing
    changes made to f3e854b607c15a9d55da2fe242039e39e8501c7b.

--- betterRP/Plugin.cs
+++ betterRP/Plugin.cs
@@ -14,6 +14,10 @@ namespace betterRP
     public class Plugin : Plugin<Config>
     {
         public override string Name { get; } = "betterRP";
+<<<<<<< HEAD
+        public override string Author { get; } = ".fkn_goose";
+        public override Version Version => new Version(0, 5, 0);
+=======
         public override string Author { get; } = ".fkn_goose & Mydak";
         public override Version Version => new Version(0, 3, 4);
 
@@ -23,12 +27,16 @@ namespace betterRP
 
         //Для передачи объекта класса Plugin и получения с него конфигов для команд
         //public CommandMethods commandMethods;
+>>>>>>> parent of 8bf8491 (Merge branch 'master' into devM)
         public static readonly Lazy<Plugin> LazyInstance = new Lazy<Plugin>(valueFactory: () => new Plugin());
         public static Plugin PluginItem => LazyInstance.Value;
         private PlayerNames PlayerNames;
         private PlayerResize PlayerResize;
+<<<<<<< HEAD
         private ReplaceSCP ReplaceSCP;
+=======
         private GrenadesAdditionalEffects GrenadesAdditionalEffects;
+>>>>>>> parent of 8bf8491 (Merge branch 'master' into devM)
         public override void OnEnabled()
         {
             PlayerNames = new PlayerNames();
@@ -36,28 +44,32 @@ namespace betterRP
             //ReplaceSCP = new ReplaceSCP();
             PlayerEv.ChangedRole += PlayerNames.OnChangedRole;
             PlayerEv.ChangedRole += PlayerResize.OnChangedRoleEventArgs;
+<<<<<<< HEAD
             //PlayerEv.SyncingData += PlayerNames.OnSyncingData;
             //PlayerEv.Left += ReplaceSCP.OnDisconnect;
+=======
             PlayerEv.Hurting += GrenadesAdditionalEffects.OnDamage;
             MapEv.ExplodingGrenade += GrenadesAdditionalEffects.OnFlash;
-            PlayerEv.Hurting += GrenadesAdditionalEffects.OnDamage;
-            MapEv.ExplodingGrenade += GrenadesAdditionalEffects.OnFlash;
+>>>>>>> parent of 8bf8491 (Merge branch 'master' into devM)
             base.OnEnabled();
         }
         public override void OnDisabled()
         {
             PlayerEv.ChangedRole -= PlayerNames.OnChangedRole;
             PlayerEv.ChangedRole -= PlayerResize.OnChangedRoleEventArgs;
+<<<<<<< HEAD
             //PlayerEv.SyncingData -= PlayerNames.OnSyncingData;
             //PlayerEv.Left -= ReplaceSCP.OnDisconnect;
             PlayerNames = null;
             PlayerResize = null;
             //ReplaceSCP = null;
+=======
             PlayerEv.Hurting -= GrenadesAdditionalEffects.OnDamage;
             MapEv.ExplodingGrenade -= GrenadesAdditionalEffects.OnFlash;
             PlayerNames = null;
             PlayerResize = null;
             GrenadesAdditionalEffects = null;
+>>>>>>> parent of 8bf8491 (Merge branch 'master' into devM)
             base.OnDisabled();
         }
     }
