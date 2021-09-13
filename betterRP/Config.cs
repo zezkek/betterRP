de60744b92f18bcfb0cb7e81312be0b5cd12a098
zezkek <super.vec2013@gmail.com>
2021-09-13 19:45:05 +0300

    wrode norm

--- betterRP/Config.cs
+++ betterRP/Config.cs
@@ -11,16 +11,20 @@ namespace betterRP
     public class Config : IConfig
     {
         public bool IsEnabled { get; set; } = true;
+        [Description("Enable or disable debug mode")]
+        public bool Debug { get; set; } = false;
         [Description("Enable or disable name change")]
         public bool PlayerNamesEnabled { get; set; } = true;
         [Description("Enable or disable player resize")]
         public bool PlayerResizeEnabled { get; set; } = true;
         [Description("New name hint message time")]
         public float HintDisplayTime { get; set; } = 10;
-        [Description("Jumping drains stamina")]
-        public float JumpDrainStamina { get; set; } = 20;
+        /* Отключено до исправления
+         * [Description("Distance between players and receivers of DoMessage")]
+         * public float DoDistance { get; set; } = 10;
+         */
         [Description("New Science Names")]
-        public List<string> ScientistAndSecuritySurname { get; set; } = new List<string> { 
+        public List<string> ScientistAndSecuritySurname { get; set; } = new List<string> {
             "Адамс","Элисон",
             "Андерсон","Бейкер",
             "Барретт","Белл",
--- betterRP/Plugin.cs
+++ betterRP/Plugin.cs
@@ -1,49 +1,60 @@
-﻿using betterRP.Handlers;
-using Exiled.API.Features;
+﻿using Exiled.API.Features;
 using Exiled.Events.EventArgs;
-using Exiled.Events.Handlers;
 using RolePlayNames.Handlers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 using PlayerEv = Exiled.Events.Handlers.Player;
+using MapEv = Exiled.Events.Handlers.Map;
 
 namespace betterRP
 {
     public class Plugin : Plugin<Config>
     {
         public override string Name { get; } = "betterRP";
-        public override string Author { get; } = ".fkn_goose";
-        public override Version Version => new Version(0, 5, 0);
+        public override string Author { get; } = ".fkn_goose & Mydak";
+        public override Version Version => new Version(0, 3, 4);
+
+        private readonly List<MEC.CoroutineHandle> coroutines = new List<MEC.CoroutineHandle>();
+        public void AddCoroutine(MEC.CoroutineHandle coroutineHandle) => coroutines.Add(coroutineHandle);
+        public void NewCoroutine(IEnumerator<float> coroutine, MEC.Segment segment = MEC.Segment.Update) => coroutines.Add(MEC.Timing.RunCoroutine(coroutine, segment));
+
+        //Для передачи объекта класса Plugin и получения с него конфигов для команд
+        //public CommandMethods commandMethods;
         public static readonly Lazy<Plugin> LazyInstance = new Lazy<Plugin>(valueFactory: () => new Plugin());
         public static Plugin PluginItem => LazyInstance.Value;
         private PlayerNames PlayerNames;
         private PlayerResize PlayerResize;
-        private ReplaceSCP ReplaceSCP;
         public override void OnEnabled()
         {
+            Log.Info("betterRP enabled");
+            //commandMethods = new CommandMethods(this);
             PlayerNames = new PlayerNames();
             PlayerResize = new PlayerResize();
-            //ReplaceSCP = new ReplaceSCP();
             PlayerEv.ChangedRole += PlayerNames.OnChangedRole;
             PlayerEv.ChangedRole += PlayerResize.OnChangedRoleEventArgs;
-            //PlayerEv.SyncingData += PlayerNames.OnSyncingData;
-            //PlayerEv.Left += ReplaceSCP.OnDisconnect;
             base.OnEnabled();
         }
         public override void OnDisabled()
         {
+            //commandMethods = null;
+            if (coroutines != null && coroutines.Count > 0) MEC.Timing.KillCoroutines(coroutines.ToArray());
+
             PlayerEv.ChangedRole -= PlayerNames.OnChangedRole;
             PlayerEv.ChangedRole -= PlayerResize.OnChangedRoleEventArgs;
-            //PlayerEv.SyncingData -= PlayerNames.OnSyncingData;
-            //PlayerEv.Left -= ReplaceSCP.OnDisconnect;
             PlayerNames = null;
             PlayerResize = null;
-            //ReplaceSCP = null;
             base.OnDisabled();
+
+            if (Config.IsEnabled)
+            {
+                Log.Info("betterRP disabled");
+                base.OnDisabled();
+            }
         }
     }
 }
